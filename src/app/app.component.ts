ef26e630e550c5485db19fd9b621c89c9e11ec7b
Omar Dulaimi <omar@eila.io>
2019-07-23 23:01:45 +0300

    add drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'JigsawPuzzle';
+
+  puzzle = [
+    '../assets/images/puzzle/row-2-col-3.jpg',
+    '../assets/images/puzzle/row-2-col-1.jpg',
+    '../assets/images/puzzle/row-2-col-2.jpg',
+    '../assets/images/puzzle/row-1-col-1.jpg',
+    '../assets/images/puzzle/row-1-col-2.jpg',
+    '../assets/images/puzzle/row-1-col-3.jpg',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.puzzle, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatGridListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
